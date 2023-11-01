d7fca5ce643893aefa6877c36848875f4d4ffbd8
Justin Schroeder <justin@formkit.com>
2023-11-01 11:55:00 -0400

    feat: adds url rehydration

--- pages/editor.vue
+++ pages/editor.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import type { Theme, ThemeOptions } from "@formkit/theme-creator"
 import { rootClasses } from "@formkit/theme-creator"
+import { slugify } from "@formkit/utils"
 import { FormKitProvider, defaultConfig, FormKitKitchenSink } from '@formkit/vue';
 import { genesisIcons } from '@formkit/icons';
 import { createProPlugin, inputs } from "@formkit-enterprise/pro";
@@ -9,10 +10,26 @@ import { themeManifest } from '~/src/manifest';
 const config = ref()
 onMounted(async () => {
   const route = useRoute()
+  const router = useRouter()
   const themeName = route.query.theme || 'regenesis'
   const variables = parseVariables(route.query.variables as string || '')
   const t = themeName
   const pro = createProPlugin(useRuntimeConfig().public.formkitProKey, inputs);
+  document.addEventListener('formkit-theme-update', ((e: CustomEvent) => {
+    const { variables, theme } = e.detail
+    let vars = ''
+    let i = 0;
+    for (const variable in variables) {
+      vars += `${i++ ? ',' : ''}${variable}=${variables[variable]}`
+    }
+    router.replace({
+      query: {
+        ...route.query,
+        theme: slugify(theme.meta.name),
+        variables: vars
+      }
+    })
+  }) as EventListener)
   if (typeof t === 'string' && t in themeManifest) {
     const theme = await themeManifest[t as keyof typeof themeManifest]()
     config.value = defaultConfig({
@@ -22,8 +39,10 @@ onMounted(async () => {
         rootClasses: rootClasses(theme.default(variables).tailwind()),
       }
     })
-    const { createEditor } = await import('@formkit/theme-editor')
-    createEditor()
+    if (!customElements.get('formkit-theme-editor')) {
+      const { createEditor } = await import('@formkit/theme-editor')
+      createEditor() // create the editor dialog
+    }
   }
 })
 </script>
